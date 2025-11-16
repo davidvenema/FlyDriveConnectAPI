f0e8245673a274422c5dd5f33b3a1ad189e49d2d
David Venema <44187356+davidvenema@users.noreply.github.com>
2025-11-16 13:48:31 +1100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,9 @@ set -e
 
 echo "From start.sh: Starting FastAPI with uvicorn..."
 
+# App Runner's default Python environment often puts installed packages here.
+# Source this path to ensure Uvicorn is in the PATH/environment.
+source /usr/local/bin/activate || true
+
 # Run using the same python as App Runner's runtime
 exec python3 -m uvicorn main:app --host 0.0.0.0 --port 8080
