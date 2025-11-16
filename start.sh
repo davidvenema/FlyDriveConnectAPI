ba2d0c25e0d35735f91efcaf49f45b6e8f1004a2
David Venema <44187356+davidvenema@users.noreply.github.com>
2025-11-16 22:58:04 +1100

    Update start.sh
    Setting it up as a virtual env

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -e
 
-echo "From start.sh: Setting PYTHONPATH and executing uvicorn module."
+echo "From start.sh: Activating venv and Starting uvicorn..."
 
-# Explicitly add the site-packages directory to PYTHONPATH where the 'uvicorn' module resides.
-# This bypasses any system PATH issues.
-export PYTHONPATH="/usr/local/lib/python3.11/site-packages:$PYTHONPATH"
+# App Runner sets WORKDIR to /app//, but be explicit:
+cd /app
 
-# Run uvicorn using the python module directly, which should now be discoverable.
-# We remove 'exec' to ensure the shell handles the script fully before starting the process.
-python3 -m uvicorn main:app --host 0.0.0.0 --port 8080
+# Activate the virtual environment that we create in the build step
+source .venv/bin/activate
+
+# Run uvicorn from the venv
+exec uvicorn main:app --host 0.0.0.0 --port 8080
